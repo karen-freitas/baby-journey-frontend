5e4c46a8033551059e9ae71d1cfa895bc1bba388
Karen Freitas <karenfcorrea@gmail.com>
2025-04-25 20:49:40 -0300

    add footer component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Carousel from './components/Carousel';
+import Header from './components/Header';
+import './css/base.css'
+import './css/embla.css'
+import './css/sandbox.css'
+
+
+const OPTIONS = { dragFree: true, loop: true }
+const SLIDE_COUNT = 5
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+    <Carousel slides={SLIDES} options={OPTIONS} />
+ 
+  </>
   );
 }
 
